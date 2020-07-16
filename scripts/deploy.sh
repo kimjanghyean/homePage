9f71307111ec8fd058967b8e1efdac76e36de53a
kimjanghyean <mskjh6@naver.com>
2020-07-16 22:47:17 +0900

    rere

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,28 +2,26 @@
 
 REPOSITORY=/home/ec2-user/app/step6
 
-echo "> 현재 구동중인 애플리케이션 pid 확인"
+echo "> $REPOSITORY 디렉터리로 이동"
+cd $REPOSITORY
 
-CURRENT_PID=$(pgrep -f homepage)
+echo "> 배포 파일 복사"
+cp $REPOSITORY/zip/*.war $REPOSITORY/
 
-echo "$CURRENT_PID"
+echo "> 구동 중인 애플리케이션 PID 검색"
+CURRENT_PID=$(pgrep -fl 'homepage*' | grep war | awk '{ print $1 }')
 
-if [ -z $CURRENT_PID ]; then
-    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
+echo "> 구동 중인 애플리케이션 PID : $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동 중인 애플리케이션이 없습니다."
+else 
     echo "> kill -15 $CURRENT_PID"
     kill -15 $CURRENT_PID
     sleep 5
 fi
 
-echo "> 새 어플리케이션 배포"
-
-echo "> Build 파일 복사"
-
-cp $REPOSITORY/build/build/libs/*.jar $REPOSITORY/jar/
-
-JAR_NAME=$(ls $REPOSITORY/jar/ |grep 'homepage' | tail -n 1)
-
-echo "> JAR Name: $JAR_NAME"
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep war | tail -n 1)
+echo "> 새 애플리케이션($JAR_NAME) 배포"
 
 nohup java -jar $REPOSITORY/jar/$JAR_NAME &
